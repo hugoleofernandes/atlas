18bc0022d3e32b5c2545b47bd5b3fd50cbae8cd4
hugo <hugoleofernandes@gmail.com>
2026-06-15 11:31:52 +0100

    refactor: hangfire removed

--- source-code/src/Outbox/Atlas.Outbox.Infrastructure/Hangfire/HangfireOutboxQueues.cs
+++
@@ -1,7 +0,0 @@
-namespace Atlas.Outbox.Infrastructure.Hangfire;
-
-public static class HangfireOutboxQueues
-{
-    public const string Identity = "identity-outbox";
-    public const string Staff = "staff-outbox";
-}
